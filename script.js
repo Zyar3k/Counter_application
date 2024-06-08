037dc1f302b526243ba701936820beb389f4e65e
Arkadiusz Zygan <a.zygan@gmail.com>
2024-06-08 20:29:40 +0200

    Add logic to project

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Start")
+document.getElementById("incBtn").addEventListener("click", incCounter);
+document.getElementById("decBtn").addEventListener("click", decCounter);
+document.getElementById("resetBtn").addEventListener("click", resetCounter);
+
+let counterDisplay = document.getElementById("counterDisplay");
+let counterValue = 0;
+
+
+function updateCounterDisplay(){
+    counterDisplay.textContent = counterValue;
+}
+
+
+function incCounter(){
+    counterValue++;
+    updateCounterDisplay();
+    }
+    function decCounter(){
+        if(counterValue>0){
+
+            counterValue--;
+        updateCounterDisplay();
+        }
+        }
+
+        function resetCounter(){
+            counterValue = 0
+            updateCounterDisplay();
+}
